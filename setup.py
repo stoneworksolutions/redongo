fe61ef9bfadfdafc551ea3f3588eca509dd50c3c
apc87 <antoniopalomocardenas@gmail.com>
2015-09-18 12:41:59 +0200

    New tag

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ DIRS_EXCLUDED = ['dist', 'build', 'docs', 'tests']
 setup(
   name = 'redongo',
   packages = find_packages(exclude=DIRS_EXCLUDED),
-  version = '0.1.7',
+  version = '0.1.8',
   description = 'Gets stuff from a Redis queue and inserts it in Mongo',
   author = 'StoneWork Solutions',
   author_email = 'dev@stoneworksolutions.net',
   url = 'https://github.com/stoneworksolutions/redongo',
-  download_url = 'https://github.com/stoneworksolutions/redongo/tarball/0.1.7',
+  download_url = 'https://github.com/stoneworksolutions/redongo/tarball/0.1.8',
   keywords = ['redis', 'mongo', 'bulks'], # arbitrary keywords
   classifiers = [],
   install_requires = [
