45a0b7a8a0daa48f9b403d7751c04a0c6301e2b4
apc87 <apalomo@stoneworksolutions.net>
2015-09-18 13:43:51 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DIRS_EXCLUDED = ['dist', 'build', 'docs', 'tests']
 setup(
   name = 'redongo',
   packages = find_packages(exclude=DIRS_EXCLUDED),
-  version = '0.1.8',
+  version = '0.1.8.1',
   description = 'Gets stuff from a Redis queue and inserts it in Mongo',
   author = 'StoneWork Solutions',
   author_email = 'dev@stoneworksolutions.net',
