4c4ff7c9e7a9c3ffb18b27a870f8456b223dfb7a
apc87 <antoniopalomocardenas@gmail.com>
2015-10-19 11:27:36 +0200

    check if pk is none

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DIRS_EXCLUDED = ['dist', 'build', 'docs', 'tests']
 setup(
   name = 'redongo',
   packages = find_packages(exclude=DIRS_EXCLUDED),
-  version = '0.1.10.3',
+  version = '0.1.10.4',
   description = 'Gets stuff from a Redis queue and inserts it in Mongo',
   author = 'StoneWork Solutions',
   author_email = 'dev@stoneworksolutions.net',
