4b802a8120318560894d88c0840e97bf948a5e4b
nsbk <pedroten@gmail.com>
2015-10-20 12:58:27 +0200

    Added check not to install ujson if runing on PyPy

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
+import platform
 
 DIRS_EXCLUDED = ['dist', 'build', 'docs', 'tests']
 
+install_requires = [
+    'pycrypto',
+    'pymongo',
+    'redis',
+    'Twisted',
+]
+
+if platform.python_implementation() != 'PyPy':
+    install_requires.append('ujson')
+
 setup(
-  name = 'redongo',
-  packages = find_packages(exclude=DIRS_EXCLUDED),
-  version = '0.1.10.6',
-  description = 'Gets stuff from a Redis queue and inserts it in Mongo',
-  author = 'StoneWork Solutions',
-  author_email = 'dev@stoneworksolutions.net',
-  url = 'https://github.com/stoneworksolutions/redongo',
-  download_url = 'https://github.com/stoneworksolutions/redongo/tarball/0.1.10',
-  keywords = ['redis', 'mongo', 'bulks'], # arbitrary keywords
-  classifiers = [],
-  install_requires = [
-	'pycrypto',
-	'pymongo',
-	'redis',
-	'ujson',
-	'Twisted',
-  ]
+    name='redongo',
+    packages=find_packages(exclude=DIRS_EXCLUDED),
+    version='0.1.10.7',
+    description='Gets stuff from a Redis queue and inserts it in Mongo',
+    author='StoneWork Solutions',
+    author_email='dev@stoneworksolutions.net',
+    url='https://github.com/stoneworksolutions/redongo',
+    download_url='https://github.com/stoneworksolutions/redongo/tarball/0.1.10',
+    keywords=['redis', 'mongo', 'bulks'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=install_requires
 )
